725e573bb76696609dd53e6cc6b8e71b464c48a9
Ruben Koker <ruben.koker@itvitaelearning.nl>
2025-01-14 16:32:21 +0100

    user managemetn page added

--- Users.php
+++ Users.php
@@ -6,13 +6,26 @@ include('includes/functions.php');
 secure();
 include('includes/header.php');
 
-
-
+if($stm = $connect->prepare('SELECT * FROM users')){
+    $stm->execute();
+    $result = $stm->get_result();
+    $user = $result->fetch_assoc();
+    var_dump($user);
+    die();
+    
+    if($user){
+        
+    }
+    $stm->close();
+}
+else{
+  echo 'could not prepare statement';
+}
 ?>
 <div class="container mt-5">
 <div class="row justify-content-center">
   <div class="col-md-6">
-    <h1 class="display-1">Dashboard</h1>
+    <h1 class="display-1">User managemetn</h1>
     <a href="users.php">Users management</a>
     <a href="posts.php">Posts management</a>
 </div>
